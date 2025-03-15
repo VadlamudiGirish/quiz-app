ae2906582c9ae058715c0ec00ff23833c79e4fba
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-15 19:25:20 +0100

    Implement form with the letter count

--- js/form.js
+++ js/form.js
@@ -8,9 +8,33 @@ let cardsData = [];
 const formElement = document.querySelector("[js-data='form-element']");
 const cardSection = document.querySelector("[js-data='card-section']");
 
+const attachCharCounters = () => {
+  const fields = formElement.querySelectorAll("textarea, input[type='text']");
+
+  fields.forEach((field) => {
+    const maxLengthAttr = field.getAttribute("maxlength");
+    const maxValue = Number(maxLengthAttr);
+
+    const counter = document.createElement("span");
+    counter.classList.add("char-count");
+
+    const remaining = maxValue - field.value.length;
+    counter.textContent = `${remaining} characters remaining`;
+    field.parentNode.insertBefore(counter, field.nextSibling);
+
+    field.addEventListener("input", () => {
+      const remaining = maxValue - field.value.length;
+      counter.textContent = `${remaining} character${
+        remaining === 1 ? "" : "s"
+      } remaining`;
+    });
+  });
+};
+
+attachCharCounters();
+
 function renderCards() {
   cardSection.innerHTML = "";
-
   cardsData.forEach((data) => {
     const cardHTML = createCard(
       data.isBookmarked,
@@ -19,7 +43,6 @@ function renderCards() {
       showText,
       data.tag
     );
-
     cardSection.insertAdjacentHTML("beforeend", cardHTML);
   });
 
@@ -39,7 +62,6 @@ function renderCards() {
     .forEach((button, index) => {
       button.addEventListener("click", () => {
         cardsData[index].isBookmarked = !cardsData[index].isBookmarked;
-
         renderCards();
       });
     });
@@ -47,17 +69,24 @@ function renderCards() {
 
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const formData = new FormData(e.target);
   const data = Object.fromEntries(formData);
 
   if (data.question && data.answer) {
     data.isBookmarked = false;
     cardsData.push(data);
-
     renderCards();
   }
 
   e.target.reset();
   e.target.elements.question.focus();
+
+  const fields = formElement.querySelectorAll("textarea, input[type='text']");
+  fields.forEach((field) => {
+    const maxValue = Number(field.getAttribute("maxlength"));
+    const counter = field.nextElementSibling;
+    if (counter && counter.classList.contains("char-count")) {
+      counter.textContent = `${maxValue} characters remaining`;
+    }
+  });
 });
